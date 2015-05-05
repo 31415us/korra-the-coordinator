bbc8f2e56da65ee402ff944aca0349ab1a10b22f
31415us <pius.vondaeniken@epfl.ch>
2015-05-05 23:40:22 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+from setuptools import setup
+
+args = dict(
+        name='korra',
+        version='0.1',
+        description='coordinates robot actions',
+        packages=['korra'],
+        install_requires=['pykka', 'molly'],
+        author='Pius von Daeniken',
+        url='https://github.com/cvra/korra-the-coordinator'
+)
+
+setup(**args)
