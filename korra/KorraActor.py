878821cf5ede9122d1cf626095d71795de8fe928
31415us <pius.vondaeniken@epfl.ch>
2015-05-20 17:36:59 +0200

    publish trajectories regularly

--- korra/KorraActor.py
+++ korra/KorraActor.py
@@ -10,16 +10,23 @@ class KorraActor(pykka.ThreadingActor):
         self.state_pub = state_pub
         self.env = env
         self.molly = molly
-        self.molly_timer = TimerActor(self, 0.1, 'molly_timer')
+        self.molly_timer = TimerActor.start(self, 0.1, 'molly_timer')
         self.molly.tell({'cmd' : 'init', 'robot_state' : robot_init_state})
 
+        self.publish_timer = TimerActor.start(self, 0.01, 'publish_timer')
+
         self.molly_future = None
+
+        self.molly_timer.tell({'cmd' : 'start'})
+        self.publish_timer.tell({'cmd' : 'start'})
 
     def on_receive(msg):
         cmd = msg.get('cmd')
 
         if cmd == 'molly_timer':
             self.molly.tell({'cmd' : 'send_traj'})
+        elif cmd == 'publish_timer':
+            self.state_pub.tell({'cmd' : 'publish'})
         elif cmd == 'molly_target':
             target = cmd.get('target')
             nmsg = {
