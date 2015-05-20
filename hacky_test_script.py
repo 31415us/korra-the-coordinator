53273bc37ba53b97672acbd8a520e60ff0910c9d
Antoine Albertelli <antoinea101@gmail.com>
2015-05-20 23:40:01 +0200

    pep8 hacky script

--- hacky_test_script.py
+++ hacky_test_script.py
@@ -1,4 +1,3 @@
-
 import pykka
 import time
 
@@ -12,8 +11,8 @@ from korra.TimerActor import TimerActor
 from molly.Vec2D import Vec2D
 from pickit.Datatypes import RobotSpacePoint
 
-class Tester(object):
 
+class Tester(object):
     def __init__(self):
         self.env = EnvironmentActor.start()
         self.state_pub = StatePublisherActor.start(('localhost', 20000))
@@ -21,30 +20,31 @@ class Tester(object):
         self.right_arm = ArmActor.start(self.state_pub, self.env, 0.1, 'right')
         self.left_arm = ArmActor.start(self.state_pub, self.env, 0.1, 'left')
 
-        self.korra = KorraActor.start(self.state_pub, self.env, self.molly, self.right_arm, self.left_arm)
+        self.korra = KorraActor.start(self.state_pub, self.env, self.molly,
+                                      self.right_arm, self.left_arm)
 
         self.molly_timer = TimerActor.start(self.korra, 0.2, 'molly_timer')
         self.pub_timer = TimerActor.start(self.korra, 0.1, 'publish_timer')
 
     def init_explicit(self, init_state):
-        self.molly.tell({'cmd' : 'init', 'robot_state' : init_state})
+        self.molly.tell({'cmd': 'init', 'robot_state': init_state})
         self.right_arm.tell({
-                             'cmd' : 'init',
-                             'right_shoulder_state' : (0, 0),
-                             'right_elbow_state' : (0, 0),
-                             'right_wrist_state' : (0, 0),
-                             'right_z_state' : (0, 0),
-                             })
+            'cmd': 'init',
+            'right_shoulder_state': (0, 0),
+            'right_elbow_state': (0, 0),
+            'right_wrist_state': (0, 0),
+            'right_z_state': (0, 0),
+        })
         self.left_arm.tell({
-                             'cmd' : 'init',
-                             'left_shoulder_state' : (0, 0),
-                             'left_elbow_state' : (0, 0),
-                             'left_wrist_state' : (0, 0),
-                             'left_z_state' : (0, 0),
-                             })
+            'cmd': 'init',
+            'left_shoulder_state': (0, 0),
+            'left_elbow_state': (0, 0),
+            'left_wrist_state': (0, 0),
+            'left_z_state': (0, 0),
+        })
         time.sleep(0.2)
-        self.molly_timer.tell({'cmd' : 'start'})
-        self.pub_timer.tell({'cmd' : 'start'})
+        self.molly_timer.tell({'cmd': 'start'})
+        self.pub_timer.tell({'cmd': 'start'})
 
     def set_target(self, x, y):
         target = Vec2D(x, y)
@@ -68,7 +68,7 @@ class Tester(object):
         self.korra.tell(msg)
 
     def stop(self):
-        self.korra.tell({'cmd' : 'ramp_down'})
+        self.korra.tell({'cmd': 'ramp_down'})
 
     def is_alive(self):
         print('env', self.env.is_alive())
@@ -80,6 +80,7 @@ class Tester(object):
 
     def shutdown(self):
         pykka.ActorRegistry.stop_all()
+
 
 if __name__ == "__main__":
     t = Tester()
