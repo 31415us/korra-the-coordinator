dd811a973a473dcae7f14f00d6fc1c03ee6b7a3b
31415us <pius.vondaeniken@epfl.ch>
2015-05-20 03:34:49 +0200

    use explicit messages for everything related to molly

--- korra/KorraActor.py
+++ korra/KorraActor.py
@@ -5,30 +5,35 @@ from korra.TimerActor import TimerActor
 
 class KorraActor(pykka.ThreadingActor):
 
-    def __init__(self, state_pub, env, molly):
+    def __init__(self, state_pub, env, molly, robot_init_state):
         super(KorraActor, self).__init__()
         self.state_pub = state_pub
         self.env = env
         self.molly = molly
         self.molly_timer = TimerActor(self, 0.1, 'molly_timer')
+        self.molly.tell({'cmd' : 'init', 'robot_state' : robot_init_state})
+
+        self.molly_future = None
 
     def on_receive(msg):
         cmd = msg.get('cmd')
 
         if cmd == 'molly_timer':
             self.molly.tell({'cmd' : 'send_traj'})
-        elif cmd == 'set_target':
+        elif cmd == 'molly_target':
             target = cmd.get('target')
             nmsg = {
                     'cmd' : 'update_target',
-                    'target' : target
+                    'target' : target,
+                    'planner' : 'molly'
                     }
             self.env.tell(nmsg)
             self.molly.tell({'cmd' : 'send_traj'})
         elif cmd == 'ramp_down':
             nmsg = {
                     'cmd' : 'update_target',
-                    'target' : None
+                    'target' : None,
+                    'planner' : 'molly'
                     }
             self.env.tell(nmsg)
             self.molly.tell({'cmd' : 'send_traj'})
